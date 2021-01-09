a6355641e8c5e835641f36045420e272a88b9b08
Akio333 <smulye10@gmail.com>
2021-01-09 21:55:16 +0530

    ADDED: Play of Snake, ladder and No play.

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,4 +1,7 @@
 public class SnakeAndLadder {
+    public static final int IS_LADDER = 1;
+    public static final int IS_SNAKE = 2;
+
     public static void main(String[] args) throws Exception {
         // Welcome Message
         System.out.println("Welcome To Snake and Ladder Simulator .... ");
@@ -7,11 +10,25 @@ public class SnakeAndLadder {
         // Variables
         int startPosition = 0;
         int currentPosition = 0;
+        int diceFace;
+        int play;
 
         // Computation
         System.out.println("Player is Starting at: " + startPosition);
-        int diceFace = (int) ((Math.floor(Math.random() * 10) % 6) + 1);
+        diceFace = (int) ((Math.floor(Math.random() * 10) % 6) + 1);
         System.out.println("Current Dice Face: " + diceFace);
-
+        play = (int) ((Math.floor(Math.random() * 10) % 3));
+        switch (play) {
+            case IS_LADDER:
+                currentPosition += diceFace;
+                break;
+            case IS_SNAKE:
+                currentPosition -= diceFace;
+                break;
+            default:
+                currentPosition = currentPosition + 0; // Useless line - Added For reference only.
+                break;
+        }
+        System.out.println("New Position: " + currentPosition);
     }
 }
