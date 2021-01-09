9b8f552b1fcfa31d148c1f8a3e623aaf791b46d3
Akio333 <smulye10@gmail.com>
2021-01-09 21:35:01 +0530

    ADDED: Start Position

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,6 +1,15 @@
 public class SnakeAndLadder {
     public static void main(String[] args) throws Exception {
+        // Welcome Message
         System.out.println("Welcome To Snake and Ladder Simulator .... ");
         System.out.println("************************************");
+
+        // Variables
+        int startPosition = 0;
+        int currentPosition = 0;
+
+        // Computation
+        System.out.println("Player is Starting at: " + startPosition);
+
     }
 }
