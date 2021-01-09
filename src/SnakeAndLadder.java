9f6f5846ec7136468e2419e108a1fa61fe2070c7
Akio333 <smulye10@gmail.com>
2021-01-09 19:28:57 +0530

    ADDED: Initial Welcome message.

---
+++ src/SnakeAndLadder.java
@@ -0,0 +1,6 @@
+public class SnakeAndLadder {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome To Snake and Ladder Simulator .... ");
+        System.out.println("************************************");
+    }
+}
